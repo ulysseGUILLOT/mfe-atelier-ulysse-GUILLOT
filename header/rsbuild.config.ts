9288a6a11e711fc25bde89ea2166219eac4202c0
ulysseGUILLOT <ulysseguillot@gmail.com>
2025-02-13 10:38:15 +0100

    config rsbuild

--- header/rsbuild.config.ts
+++ header/rsbuild.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginSvelte } from '@rsbuild/plugin-svelte';
+import {pluginModuleFederation} from "@module-federation/rsbuild-plugin";
 
 export default defineConfig({
-  plugins: [pluginSvelte()],
+  plugins: [
+      pluginSvelte(),
+      pluginModuleFederation({
+          name: 'federation_provider',
+          exposes: {
+              './Header': './src/Header.svelte',
+          },
+          //shared: ['svelte'],
+      }),
+  ],
 });
