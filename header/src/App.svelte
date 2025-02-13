6b1cfc7add9f70afe07a7886a48c4642d0eff7a4
ulysseGUILLOT <ulysseguillot@gmail.com>
2025-02-13 10:12:10 +0100

    add header

--- header/src/App.svelte
+++ header/src/App.svelte
@@ -1,28 +1,14 @@
+<script>
+  import Header from "./Header.svelte";
+</script>
+
 <main>
-  <div class="content">
-    <h1>Rsbuild with Svelte</h1>
-    <p>Start building amazing things with Rsbuild.</p>
-  </div>
+  <Header />
 </main>
 
 <style>
-.content {
-  display: flex;
-  min-height: 100vh;
-  line-height: 1.1;
-  text-align: center;
-  flex-direction: column;
-  justify-content: center;
-}
-
-.content h1 {
-  font-size: 3.6rem;
-  font-weight: 700;
-}
-
-.content p {
-  font-size: 1.2rem;
-  font-weight: 400;
-  opacity: 0.5;
-}
+  main {
+    background-color: #020917;
+    height: 100vh;
+  }
 </style>
---
+++ header/src/Header.svelte
@@ -0,0 +1,18 @@
+<header>
+    <div>TP Ulysse GUILLOT</div>
+    <a href="https://github.com/ulysseGUILLOT/mfe-atelier-ulysse-GUILLOT" target="_blank">github</a>
+</header>
+
+<style>
+    header {
+        background: #101725;
+        padding: 1em;
+        font-size: xx-large;
+        display: grid;
+        grid-template-columns: 90% 10%;
+    }
+
+    a {
+        color: white;
+    }
+</style>
